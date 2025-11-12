b7a7f256202cefe02763e3009c8342030c1beb32
wpceid <wpceid@gmail.com>
2025-11-12 20:03:46 -0300

    Adicionando a impressão dos objetos instanciados

--- respostas/respExercicio01.py
+++ respostas/respExercicio01.py
@@ -15,3 +15,11 @@ A2 = Aluno("João", 987654321, "Geografia")
 D1 = Disciplina("Matemática", 1, 24)
 D2 = Disciplina("Geografia", 2, 12)
 
+lista_alunos = [A1, A2]
+lista_disciplinas = [D1, D2]
+
+for aluno in lista_alunos:
+    print(f"Aluno: {aluno.nome}, Matrícula: {aluno.matricula}, Curso: {aluno.curso}")
+
+for disciplina in lista_disciplinas:
+    print(f"Disciplina: {disciplina.nome}, Código: {disciplina.codigo}, Carga Horária: {disciplina.carga_horaria}")
