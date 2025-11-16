5f1c76e5d38fe27c53c4287f29377e56330a6c61
Carina Dalpino <carina.g.santos22@gmail.com>
2025-11-16 20:41:43 -0300

    Exercicios atualizados

---
+++ respExercicio04.py
@@ -0,0 +1,33 @@
+class Pessoa:
+    def __init__(self, nome, cpf, data_nascimento):
+        self.nome = nome
+        self.cpf = cpf
+        self.data_nascimento = data_nascimento
+
+    def apresentar(self):
+       return f"Olá, meu nome é {self.nome}, meu CPF é {self.cpf}"
+
+
+class Funcionario(Pessoa):
+    def __init__(self, nome, cpf, data_nascimento, cargo):
+        super().__init__(nome, cpf, data_nascimento)
+        self.cargo = cargo
+
+
+class Tutor(Pessoa):
+    def __init__(self, nome, cpf, data_nascimento, atuacao):
+        super().__init__(nome, cpf, data_nascimento)
+        self.atuacao = atuacao
+
+    def apresentar(self):
+        super().apresentar()
+        return f"Olá, meu nome é {self.nome}, meu CPF é {self.cpf} e sou tutor na área de {self.atuacao}."
+
+# Testando as classes com polimorfismo
+
+Funcionario("João Silva", "123.456.789-00", "01/01/1990", "Secretário"),
+Tutor("Maria Santos", "987.654.321-00", "15/05/1985", "Programação")
+
+print(Funcionario.apresentar(Funcionario))
+print(Tutor.apresentar(Tutor))
+        
---
+++ respExercicio08.py
@@ -0,0 +1,40 @@
+class Departamento:
+    def __init__(self, nome, sigla):
+        self.nome = nome
+        self.sigla = sigla
+        self.professores = []
+            
+
+    @classmethod
+    def criar_departamento_exatas(cls, nome):
+        return cls(nome, "EXA")
+    
+    @classmethod
+    def criar_departamento_humanas(cls, nome):
+        return cls(nome, "HUM")
+    
+    def adicionar_professor(self, professor):
+        self.professores.append(professor)
+
+    def listar_professores(self):
+        print(f"=== Professores do departamento {self.nome}: ===")
+        for p in self.professores:
+            print(f"- {p.prof}")
+
+            if __name__ == "__main__":
+                depto_exatas = Departamento.criar_departamento_exatas("Matematica e Computação")
+                depto_humanas = Departamento.criar_departamento_humanas("Letras e Filosofia")
+
+                print(f"Departamento: {depto_exatas.nome}, Sigla: {depto_exatas.sigla}")
+                print(f"Departamento: {depto_humanas.nome}, Sigla: {depto_humanas.sigla}")
+
+                depto_exatas.adicionar_professor("Joao Silva")
+                depto_exatas.adicionar_professor("Ana Costa")
+                depto_exatas.listar_professores()
+                
+                
+    
+                
+                    
+
+            
